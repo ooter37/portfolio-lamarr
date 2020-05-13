f211b3985d2f7b289bc5e3da7906b2e63cf355b0
Derek Lamarr <derek@macbook-pro.lan>
2020-05-13 11:52:59 -0700

    fixed git ignore

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express()
 
-// app.use(express.static(path.join(__dirname, '/index.html')))
+app.use(express.static(path.join(__dirname, '/index.html')))
 
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/index.html'))
